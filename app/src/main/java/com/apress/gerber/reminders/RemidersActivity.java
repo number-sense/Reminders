aeaf6c18174e4a81274a91732ebe32aae0e6a185
Hassan Baajour <hbaajour@gsmlondon.ac.uk>
2017-05-22 17:00:57 +0100

    Initial commit

---
+++ app/src/main/java/com/apress/gerber/reminders/RemidersActivity.java
@@ -0,0 +1,13 @@
+package com.apress.gerber.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemidersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remiders);
+    }
+}
